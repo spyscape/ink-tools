f527a31a94684b3c8bd27d9d7a98cc7168c4727b
William Hooke <wrhooke@gmail.com>
2021-02-15 20:19:04 +0000

    Use real Ink example in spec

--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -3,9 +3,10 @@ const subject = require('../src/lib').compile;
 describe('#compile', () => {
   describe('given a valid ink string', () => {
     it('converts the ink to to ink json', () => {
-      const inkScript = '';
+      const inkScript = `Hello`;
+      const expectedJson = `{"inkVersion":19,"root":[["^Hello","\n",["done",{"#n":"g-0"}],null],"done",null],"listDefs":{}}`;
 
-      expect(subject(inkScript)).toEqual('{}');
+      expect(subject(inkScript)).toEqual(expectedJson);
     })
   })
 });
