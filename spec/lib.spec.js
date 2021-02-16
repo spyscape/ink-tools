ce73adeafb90e54d012c540538cad5360cd49d5e
William Hooke <wrhooke@gmail.com>
2021-02-16 11:37:19 +0000

    Implement compile function and test
    Write the input ink script to a file, execute the inklecate command with shelljs, then read the resulting file, then delete both temporary files.

--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -2,11 +2,12 @@ const subject = require('../src/lib').compile;
 
 describe('#compile', () => {
   describe('given a valid ink string', () => {
-    it('converts the ink to to ink json', () => {
-      const inkScript = `Hello`;
-      const expectedJson = `{"inkVersion":19,"root":[["^Hello","\n",["done",{"#n":"g-0"}],null],"done",null],"listDefs":{}}`;
+    it('returns the ink string as ink json', () => {
+      const inkScript = 'Hello';
+      const expectedStr = String.raw`{"inkVersion":19,"root":[["^Hello","\n",["done",{"#n":"g-0"}],null],"done",null],"listDefs":{}}`;
+      const result = subject(inkScript).trim();
 
-      expect(subject(inkScript)).toEqual(expectedJson);
+      expect(result).toEqual(expectedStr);
     })
   })
 });
--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const { v4: uuidv4 } = require('uuid');
+const { getInklecateBin } = require('./Command/get-inklecate-bin');
+
 function compile(inkScript) {
-  return inkScript;
+  const inklecateBin = getInklecateBin();
+  const uniqueId = uuidv4();
+  const inkFilePath = path.resolve(`__TEMP-INK_${uniqueId}.ink`);
+  const outputFilePath = `${inkFilePath}.json`;
+  const inklecateCmd = `${inklecateBin} ${inkFilePath}`;
+  fs.writeFileSync(inkFilePath, inkScript);
+  shell.exec(inklecateCmd);
+  const inkJson = fs.readFileSync(outputFilePath, 'utf8');
+  shell.rm(`${inkFilePath}*`);
+
+  return inkJson;
 }
 
 module.exports = {
