ff91aa705f60581a1b87ff568d63c2257142078e
William Hooke <wrhooke@gmail.com>
2021-02-16 14:49:53 +0000

    Add multi-line ink script spec

--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -1,10 +1,35 @@
+/* eslint-disable operator-linebreak */
 const subject = require('../src/lib').compile;
 
 describe('#compile', () => {
-  describe('given a valid ink string', () => {
+  describe('given a single-line ink string', () => {
     it('returns the ink string as ink json', () => {
       const inkScript = 'Hello';
       const expectedStr = String.raw`{"inkVersion":19,"root":[["^Hello","\n",["done",{"#n":"g-0"}],null],"done",null],"listDefs":{}}`;
+      const result = subject(inkScript).trim();
+
+      expect(result).toEqual(expectedStr);
+    });
+  });
+
+  describe('given a multi-line ink string', () => {
+    it('returns the ink string as ink json', () => {
+      const inkScript =
+`Hello, this is a test.
+-> test_menu
+
+== test_menu
+
+BOT: What's the plan?
+
++ [Test gadgets] -> test_gadgets
+
+== test_gadgets
+
+BOT: OK, testing gadgets
+
+->END`;
+      const expectedStr = String.raw`{"inkVersion":19,"root":[["^Hello, this is a test.","\n",{"->":"test_menu"},["done",{"#n":"g-0"}],null],"done",{"test_menu":[["^BOT: What's the plan?","\n","ev","str","^Test gadgets","/str","/ev",{"*":".^.c-0","flg":4},{"c-0":["^ ",{"->":"test_gadgets"},"\n",null]}],null],"test_gadgets":["^BOT: OK, testing gadgets","\n","end",null]}],"listDefs":{}}`;
       const result = subject(inkScript).trim();
 
       expect(result).toEqual(expectedStr);
